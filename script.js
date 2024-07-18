0fed213567e5c4c11f918346901b135d03f9750a
MattSebahar <mbsebahar1@gmail.com>
2024-07-18 15:25:49 -0600

    update js for looping game

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let computerChoice;
 let playerChoice;
+let playerScore;
+let computerScore;
 
 function getComputerChoice(){
-    number = Math.random;
+    number = Math.random();
     if(number < 0.33) {
         computerChoice = "rock";
     } else if (number < 0.66) {
@@ -19,13 +21,13 @@ function getPlayerChoice(){
 }
 }
 
-function play(){
+function playRound(){
     getComputerChoice();
     getPlayerChoice();
 
     if(playerChoice == "exit") return;
 
-    console.log(`Console chose: ${computerChoice}`)
+    console.log(`Computer chose: ${computerChoice}`)
     console.log(`You chose: ${playerChoice}`)
 
     if(playerChoice == computerChoice)
@@ -33,11 +35,31 @@ function play(){
     
     else if((playerChoice == "rock" && computerChoice == "paper") 
             || (playerChoice == "paper" && computerChoice == "scissors") 
-            || (playerChoice == "scissors" && computerChoice == "rock"))
-        console.log("Sorry, you lose!");
-    else 
-        console.log("You win!");    
+            || (playerChoice == "scissors" && computerChoice == "rock")){
+        computerScore += 1;
+        console.log("Sorry, you lose this round!");   
+    }
+    else {
+        playerScore += 1;
+        console.log("You win this round!");    
+    }
       
-    
 }
-play();
+
+function playGame(){
+    playerScore = 0;
+    computerScore = 0;
+    console.log("First to 3! Game on!")
+    while (playerScore < 3 && computerScore < 3){
+        console.log("Next Round: ")
+        playRound();
+        console.log(`The current score is Player: ${playerScore} | Computer: ${computerScore}`);
+    }
+
+    if(computerScore == 3)
+        console.log("Computer Wins!")
+    else
+        console.log("Player Wins!")
+}
+
+playGame();
